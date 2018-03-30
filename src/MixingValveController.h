cbef9c9a463f1d3c4704fd6cb3f4b5d417e7a43d
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:11:44 -0400

    Removed unnecessary header includes.

--- src/MixingValveController.h
+++ src/MixingValveController.h
@@ -7,25 +7,12 @@
 // ----------------------------------------------------------------------------
 #ifndef MIXING_VALVE_CONTROLLER_H
 #define MIXING_VALVE_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
-
-#include <SPI.h>
-#include <TLE72X.h>
-#include <EventController.h>
 
 #include <ModularClient.h>
 
