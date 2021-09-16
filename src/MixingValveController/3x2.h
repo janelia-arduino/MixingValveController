522b0e97879ae267fede7114185f9704eda189fb
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 14:10:22 -0400

    Update email address

--- src/MixingValveController/3x2.h
+++ src/MixingValveController/3x2.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef MIXING_VALVE_CONTROLLER_3X2_CONSTANTS_H
 #define MIXING_VALVE_CONTROLLER_3X2_CONSTANTS_H
--- src/MixingValveController/Constants.h
+++ src/MixingValveController/Constants.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef MIXING_VALVE_CONTROLLER_CONSTANTS_H
 #define MIXING_VALVE_CONTROLLER_CONSTANTS_H
