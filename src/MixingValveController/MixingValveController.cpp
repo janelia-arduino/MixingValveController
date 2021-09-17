24a664978e81c7c51fbbf6d56b66d37d41feb962
Peter Polidoro <peter@polidoro.io>
2021-09-17 10:58:54 -0400

    Remove spurious comments

--- src/MixingValveController/MixingValveController.cpp
+++ src/MixingValveController/MixingValveController.cpp
@@ -268,24 +268,6 @@ void MixingValveController::setMixInfo(MixingValveController::ValveValues ratio)
   mix_info_.valve_open_durations = valve_open_durations;
 }
 
-// Handlers must be non-blocking (avoid 'delay')
-//
-// modular_server_.parameter(parameter_name).getValue(value) value type must be either:
-// fixed-point number (int, long, etc.)
-// floating-point number (float, double)
-// bool
-// const char *
-// ArduinoJson::JsonArray
-// ArduinoJson::JsonObject
-// const ConstantString *
-//
-// For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
-//
-// modular_server_.property(property_name).getValue(value) value type must match the property default type
-// modular_server_.property(property_name).setValue(value) value type must match the property default type
-// modular_server_.property(property_name).getElementValue(element_index,value) value type must match the property array element default type
-// modular_server_.property(property_name).setElementValue(element_index,value) value type must match the property array element default type
-
 void MixingValveController::setValveCountHandler()
 {
   long valve_count;
